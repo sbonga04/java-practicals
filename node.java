c0ac1f3aca83d90274b498d180b6a9e31cc09249
Sbongumusa Mkhize <4323535@myuwc.ac.za>
2025-05-12 01:25:52 +0200

    Create node.java

---
+++ node.java
@@ -0,0 +1,15 @@
+//Mkhize 
+//Sbongumusa 
+//4323535
+//CSC211 _PRAC-2
+
+public class Node {
+	Patient data;
+	Node link;
+	
+	public Node(Patient val) {
+		this.data = val;
+		this.link = null;
+	}
+
+}
